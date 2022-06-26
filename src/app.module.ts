53554d8e836a1a3638bbc799f701d79644b401bb
Supanut1911 <ynotmela@gmail.com>
2022-06-26 16:49:55 +0700

    add configService-and-pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { configSerice } from './config/configService.config';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [OrderModule],
+  imports: [
+    TypeOrmModule.forRoot(configSerice.getTypeOrmConfig()),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
